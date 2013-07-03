e54112fe168d3d667f002b121cb6723674734538
Ngo Thanh Le <lent@qsoftvietnam.com>
2013-07-03 13:50:19 +0700

    1.1

---
+++ Kata1/src/main/java/StringCalculator/StringCalculator.java
@@ -0,0 +1,29 @@
+package StringCalculator;
+
+/**
+ * User: lent
+ * Date: 7/3/13
+ */
+public class StringCalculator
+{
+    public int add(String numbers)
+    {
+        if ("".equals(numbers.trim()))
+        {
+            return 0;
+        }
+        else
+        {
+            // Should be a number or numbers
+            // C1: Using regex to parse a number
+            String[] arrayNumbers = numbers.split(",");
+            // C2: Using regex to parse numbers
+            int totalValue = 0;
+            for (String number : arrayNumbers)
+            {
+                totalValue += Integer.parseInt(number);
+            }
+            return totalValue;
+        }
+    }
+}
---
+++ Kata1/src/test/java/StringCalculatorTest.java
@@ -0,0 +1,41 @@
+import StringCalculator.StringCalculator;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * User: lent
+ * Date: 7/3/13
+ */
+public class StringCalculatorTest
+{
+    StringCalculator stringCalculator;
+
+    @Before
+    public void setup()
+    {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void testAddNumbers_NoParameter()
+    {
+        int result = stringCalculator.add("");
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testAddNumbers_OneParameter()
+    {
+        int result = stringCalculator.add("1");
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testAddNumbers_TwoParameters()
+    {
+        int result = stringCalculator.add("1,2");
+        assertEquals(3, result);
+    }
+}
