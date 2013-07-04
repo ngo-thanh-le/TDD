58e28e62c58f89c390b51a830665bfae082697fd
Ngo Thanh Le <lent@qsoftvietnam.com>
2013-07-04 13:53:51 +0700

    1.5

---
+++ Kata1D2/src/main/java/StringCalculator.java
@@ -0,0 +1,63 @@
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: lent
+ * Date: 7/4/13
+ */
+public class StringCalculator
+{
+    public int add(String numbers) throws Exception
+    {
+        if ("".equals(numbers.trim()))
+        {
+            return 0;
+        }
+        else
+        {
+            String delimiter = ",|\\n";
+            // Detect new delimiter
+            if (numbers.startsWith("//"))
+            {
+                delimiter = numbers.substring(2, 3); // Support delimiter one char
+                String endingDelimiter = numbers.substring(3, 4);
+                if (!endingDelimiter.equals("\n"))
+                {
+                    throw new InvalidParameterException();
+                }
+                numbers = numbers.substring(4);
+            }
+            // Should be a number or numbers
+            // C1: Using regex to parse a number
+            String[] arrayNumbers = numbers.split(delimiter);
+            // C2: Using regex to parse numbers
+            int totalValue = 0;
+            List<String> invalidNumbers = new ArrayList<String>();
+            for (String number : arrayNumbers)
+            {
+                try
+                {
+                    Integer value = Integer.parseInt(number);
+                    if (value < 0) {
+                        invalidNumbers.add(value.toString());
+                    }
+                    totalValue += value;
+                }
+                catch (Exception e)
+                {
+                    throw new InvalidParameterException("Invalid parameter input");
+                }
+            }
+            if (invalidNumbers.size()>0) {
+                String message = "Negatives are not allowed. Wrong: ";
+                for (String invalidValue : invalidNumbers) {
+                    message += invalidValue + ",";
+                }
+                message = message.substring(0, message.length() - 1);
+                throw new Exception(message);
+            }
+            return totalValue;
+        }
+    }
+}
---
+++ Kata1D2/src/test/java/StringCalculatorTest.java
@@ -0,0 +1,107 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.security.InvalidParameterException;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * User: lent
+ * Date: 7/4/13
+ */
+public class StringCalculatorTest
+{
+    StringCalculator stringCalculator;
+
+    @Before
+    public void setup()
+    {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void testAddNumbers_EmptyString() throws Exception
+    {
+        int result = stringCalculator.add("");
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testAddNumbers_OneNumber() throws Exception
+    {
+        int result = stringCalculator.add("1");
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testAddNumbers_TwoNumbers() throws Exception
+    {
+        int result = stringCalculator.add("1,2");
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void testAddNumbers_ManyNumbers() throws Exception
+    {
+        int result = stringCalculator.add("1,2,3,4,5");
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testAddNumbers_NewLineSeparator() throws Exception
+    {
+        int result = stringCalculator.add("1\n2,3,4,5");
+        assertEquals(15, result);
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    public void testAddNumbers_NewLineSeparatorWithAnotherSeparatorCauseException() throws Exception
+    {
+        int result = stringCalculator.add("1\n,2,3,4,5");
+        fail();
+    }
+
+    @Test
+    public void testAddNumbers_CustomSeparator() throws Exception
+    {
+        int result = stringCalculator.add("//;\n1;2");
+        assertEquals(3, result);
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    public void testAddNumbers_CustomSeparatorWrong() throws Exception
+    {
+        int result = stringCalculator.add("//;1;2");
+        fail();
+    }
+
+    @Test
+    public void testAddNumbers_OneNegativeNumber()
+    {
+        try
+        {
+            int result = stringCalculator.add("-1");
+            fail();
+        }
+        catch (Exception e)
+        {
+            assertTrue(e.getMessage().equals("Negatives are not allowed. Wrong: -1"));
+        }
+    }
+
+    @Test
+    public void testAddNumbers_MultiNegativeNumbers()
+    {
+        try
+        {
+            int result = stringCalculator.add("-1,2,-3");
+            fail();
+        }
+        catch (Exception e)
+        {
+            assertTrue(e.getMessage().equals("Negatives are not allowed. Wrong: -1,-3"));
+        }
+    }
+}
