3e42aa5e11868ccab854e0ce6f90627385629287
Ngo Thanh Le <lent@qsoftvietnam.com>
2013-07-22 13:35:10 +0700

    Red 1.1

--- Kata1D2/src/test/java/StringCalculatorRemakeTest.java
+++ Kata1D2/src/test/java/StringCalculatorRemakeTest.java
@@ -23,4 +23,11 @@ public class StringCalculatorRemakeTest
         int result = stringCalculator.addNumbers("");
         assertEquals(0, result);
     }
+
+    @Test
+    public void testAddNumbers_OneNumber() throws Exception
+    {
+        int result = stringCalculator.addNumbers("1");
+        assertEquals(1, result);
+    }
 }
