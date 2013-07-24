2a0ad11dddb43fc49ff89defe5aa17bc1ed1eba0
Ngo Thanh Le <lent@qsoftvietnam.com>
2013-07-24 13:59:25 +0700

    Kata 3 - Done Layout TODO: Integrate hibernate

---
+++ BankAccountManagement/src/test/java/com/qsoft/bam/dao/AbstractDAOTest.java
@@ -0,0 +1,59 @@
+package com.qsoft.bam.dao;
+
+import org.dbunit.DataSourceDatabaseTester;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+
+/**
+ * User: lent
+ * Date: 7/24/13
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:test-context.xml"})
+@TransactionConfiguration(defaultRollback = true)
+@Transactional
+public abstract class AbstractDAOTest
+{
+    @Autowired
+    protected DataSource dataSource;
+
+    protected IDatabaseTester databaseTester;
+
+    @Before
+    public void setup() throws Exception
+    {
+        IDataSet dataSet = readDataSet();  // read data from xml file
+        cleanlyInsert(dataSet);  // empty the db and insert data
+    }
+
+    protected IDataSet readDataSet() throws Exception
+    {
+        return new FlatXmlDataSetBuilder().build(System.class.getResource("/dataset.xml"));
+    }
+
+    protected void cleanlyInsert(IDataSet dataSet) throws Exception
+    {
+        databaseTester = new DataSourceDatabaseTester(dataSource);
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+        databaseTester.setDataSet(dataSet);
+        databaseTester.onSetup();
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        databaseTester.onTearDown();
+    }
+}
